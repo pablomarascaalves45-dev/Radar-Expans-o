d6309d197deb7451fab3b4778b96b5adec23034b
pablomarascaalves45-dev <pablomarascaalves45@gmail.com>
2026-04-09 14:57:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ st.set_page_config(page_title="Radar de Expansão", layout="centered")
 # CADASTRE AQUI OS USUÁRIOS PERMITIDOS
 USUARIOS_AUTORIZADOS = {
     "Pablo": "55997260245",
-    "Joao Silva": "51988888888",
-    "Maria Souza": "48977777777"
+    "Lucas": "5499206706",
+    "Estevam": "5496964416"
+    "Eduardo": "5499865084"
+    "Eduardo": "5496733252"
 }
 
 if 'logado' not in st.session_state:
